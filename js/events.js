8f59dffc09d9cb09c3ac9a68ba897520f7cab7a9
Adam Walter <adam.iverson.walter@gmail.com>
2017-09-25 15:25:59 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -2,10 +2,16 @@ function getIt() {
   $('p').on('click', function() {
     alert("Hey!");
   });
-};
+}
+
+function frameIt() {
+  $('img').on('load', function() {
+    $('img').addClass("tasty");
+  });
+}
 
 $(document).ready(function(){
-
-// call functions here
+  getIt();
+  frameIt();
 
 });
